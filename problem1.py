94892351f560e52ee63b79efd0ff74bf5606d287
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2021-01-12 20:44:29 +0200

    Delete problem1.py

--- problem1.py
+++
@@ -1,8 +0,0 @@
-with open('numere1.txt', 'r') as f :
-    a=f.readline()
-    b=f.readline()
-with open('minim1.txt','w') as f :
-    if int(a)>int(b) :
-        f.write(b)
-    else: 
-        f.write(a)
